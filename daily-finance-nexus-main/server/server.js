55c8ae5dcdc5a0e72cb16b08d4d8e8c785411724
sadiqJ20 <sadiqj8227@gmail.com>
2025-07-25 22:40:39 +0530

    feat: add netlify url to cors origin

--- daily-finance-nexus-main/server/server.js
+++ daily-finance-nexus-main/server/server.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://daily-finance-nexus-1.onrender.com',
+  origin: ['https://daily-finance-nexus-1.onrender.com', 'http://localhost:5173', 'http://localhost:8080', 'https://animated-kangaroo-47005c.netlify.app'],
   credentials: true
 }));
 app.use(express.json());
